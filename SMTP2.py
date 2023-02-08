2ea2589c25fb96b91847e0daa0f0beafb33bb325
Jungbin Choi <jung12@email.unc.edu>
2023-02-08 04:04:35 -0500

    SMTP2.py Updated

--- SMTP2.py
+++ SMTP2.py
@@ -9,38 +9,58 @@ def main():
     try:
         file_name: str = sys.argv[1]
         text_file = open(file_name, "rt")
+        curr_line = ''
+        next_line = text_file.readline()
         response: str = ''
         code: str = ''
         data: bool = False
-        line: str = ' '
 
-        for line in text_file:
+        while True:
             response = ''
+            curr_line = next_line
+            next_line = text_file.readline()
 
-            if (len(line) >= 5) and (line[0:5] == "From:"):
-                if data:
-                    sys.stderr.write(".\n")
-                    response = sys.stdin.readline()
-                    code = response[:3]
-                    data = False
+            if (len(curr_line) >= 5) and (curr_line[0:5] == "From:"):
+                sys.stderr.write("MAIL FROM: " + curr_line[6:])
+                response = sys.stdin.readline()
+                code = response[:3]
+            elif (len(curr_line) >= 3) and (curr_line[0:3] == "To:"):
+                sys.stderr.write("RCPT TO: " + curr_line[4:])
+                response = sys.stdin.readline()
+                code = response[:3]
+
+                if (len(next_line) < 3) or (next_line[0:3] != "To:"):
                     sys.stderr.write(response)
-                    if code == "250" or code == "354":
+                    response = ''
+                    if code == "250":
                         pass
                     else:
+                        data = False
                         raise EOFError()
-
-                sys.stderr.write("MAIL FROM: " + line[6:])
-                response = sys.stdin.readline()
-                code = response[:3]
-            elif (len(line) >= 3) and (line[0:3] == "To:"):
-                sys.stderr.write("RCPT TO: " + line[4:])
-                response = sys.stdin.readline()
-                code = response[:3]
-            else:
-                if not data:
-                    data = True
                     sys.stderr.write("DATA\n")
-                sys.stderr.write(line)
+                    data = True
+                    response = sys.stdin.readline()
+                    sys.stderr.write(response)
+                    code = response[:3]
+                    if code == "354":
+                        pass
+                    else:
+                        data = False
+                        raise EOFError()
+                    response = ''
+                    if (len(next_line) >= 5) and (next_line[0:5] == "From:"):
+                        sys.stderr.write(".\n")
+                        data = False
+                        response = sys.stdin.readline()
+                        code = response[:3]
+            else:
+                sys.stderr.write(curr_line)
+
+                if (len(next_line) >= 5) and (next_line[0:5] == "From:"):
+                    sys.stderr.write(".\n")
+                    data = False
+                    response = sys.stdin.readline()
+                    code = response[:3]
 
             if response != '':
                 sys.stderr.write(response)
@@ -49,21 +69,15 @@ def main():
                 else:
                     raise EOFError()
 
-    except EOFError:
-        if data:
-            sys.stderr.write(".\n")
-            response = sys.stdin.readline()[:3]
-            sys.stderr.write(response)
-        sys.stderr.write("QUIT\n")
-        return
+            if next_line == '':
+                raise EOFError()
 
-    except StopIteration:
+    except (EOFError, IndexError):
         if data:
             sys.stderr.write(".\n")
-            response = sys.stdin.readline()[:3]
+            response = sys.stdin.readline()
             sys.stderr.write(response)
         sys.stderr.write("QUIT\n")
-        return
 
     except IOError:
         return
--- SMTP2_Test.py
+++ SMTP2_Test.py
@@ -5,67 +5,83 @@
 import sys
 
 
-# Globals
-string: str = ''
-value: str = ''
-index: int = 0
-reverse: str = ''
-forward: str = []
-
-
 def main():
-    global string, value, index, reverse, forward
     try:
         file_name: str = sys.argv[1]
-        text_file = open(file_name, 'rt')
+        text_file = open(file_name, "rt")
+        curr_line = ''
+        next_line = text_file.readline()
         response: str = ''
         code: str = ''
         data: bool = False
-        for line in text_file:
+
+        while True:
             response = ''
-            if (len(line) >= 5) and (line[0:5] == "From:"):
-                if data:
-                    sys.stderr.write(".\n")
-                    response = sys.stdin.readline()
-                    code = response[:3]
-                    data = False
-                    if code != "250" or code != "354":
+            curr_line = next_line
+            next_line = text_file.readline()
+
+            if (len(curr_line) >= 5) and (curr_line[0:5] == "From:"):
+                sys.stderr.write("MAIL FROM: " + curr_line[6:])
+                response = sys.stdin.readline()
+                code = response[:3]
+            elif (len(curr_line) >= 3) and (curr_line[0:3] == "To:"):
+                sys.stderr.write("RCPT TO: " + curr_line[4:])
+                response = sys.stdin.readline()
+                code = response[:3]
+
+                if (len(next_line) < 3) or (next_line[0:3] != "To:"):
+                    sys.stderr.write(response)
+                    response = ''
+                    if code == "250":
                         pass
                     else:
+                        data = False
                         raise EOFError()
-                    sys.stderr.write(response)
-
-                sys.stderr.write("MAIL FROM: " + line[6:])
-                response = sys.stdin.readline()
-                code = response[:3]
-            elif (len(line) >= 3) and (line[0:3] == "To:"):
-                sys.stderr.write("RCPT TO: " + line[4:])
-                response = sys.stdin.readline()
-                code = response[:3]
-            else:
-                if not data:
-                    data = True
                     sys.stderr.write("DATA\n")
-                sys.stderr.write(line)
+                    data = True
+                    response = sys.stdin.readline()
+                    sys.stderr.write(response)
+                    code = response[:3]
+                    if code == "354":
+                        pass
+                    else:
+                        data = False
+                        raise EOFError()
+                    response = ''
+                    if (len(next_line) >= 5) and (next_line[0:5] == "From:"):
+                        sys.stderr.write(".\n")
+                        data = False
+                        response = sys.stdin.readline()
+                        code = response[:3]
+            else:
+                sys.stderr.write(curr_line)
+
+                if (len(next_line) >= 5) and (next_line[0:5] == "From:"):
+                    sys.stderr.write(".\n")
+                    data = False
+                    response = sys.stdin.readline()
+                    code = response[:3]
 
             if response != '':
                 sys.stderr.write(response)
-                if code != "250" or code != "354":
+                if code == "250" or code == "354":
                     pass
                 else:
                     raise EOFError()
 
+            if next_line == '':
+                raise EOFError()
+
     except (EOFError, IndexError):
         if data:
-            sys.stderr.write(".\n")
-            response = sys.stdin.readline()[:3]
-            sys.stderr.write(response)
+            sys.stderr.write("\n.\n")
+            response = sys.stdin.readline()
+            sys.stderr.write(response[:3] + '\n')
         sys.stderr.write("QUIT\n")
-        return
-    
+
     except IOError:
         return
-    
+
     finally:
         text_file.close()
 
